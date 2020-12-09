4af8e58b3726cb15dc71cbaaf1dd299bb18d402f
MaelleH <h.maelle.98@gmail.com>
2020-12-09 14:39:36 +0100

    server

--- server.js
+++ server.js
@@ -2,43 +2,30 @@
 
 let Santa = require('./santa_class.js');
 
-let sweden = {	
-		latitude:67.498454,
-		longitude:21.040181
-	};
+let sweden = {
+	latitude:67.498454,
+	longitude:21.040181
+};
 
 let mySanta = new Santa(sweden);
-const PORT = process.env.PORT || 3000;
-const INDEX = '/main_app_page.html';
 
 
 const express = require('express');
-const riveScript = require('rivescript');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/main_app_page.html';
 
 const server = express()
 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-const io = require('socket.io')(server, {
-	handlePreflightRequest: function (req, res) {
-		var headers = {
-			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-			'Access-Control-Allow-Origin': 'http://localhost:3000',
-			'Access-Control-Allow-Credentials': true
-		};
-		res.writeHead(200, headers);
-		res.end();
-	}
-});
-
-
-
-
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+	console.log('Client connected');
+	socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval( () => io.emit('time', new Date().toTimeString()), 1000);
