23f371af60bdf6fbec55ed1098a17a1671bc7b96
MaelleH <h.maelle.98@gmail.com>
2020-12-09 14:49:34 +0100

    server

--- server.js
+++ server.js
@@ -23,12 +23,11 @@ const server = express()
 
 const io = socketIO(server);
 
+
 io.on('connection', (socket) => {
 	console.log('Client connected');
 	socket.on('disconnect', () => console.log('Client disconnected'));
 });
-
-setInterval( () => io.emit('time', new Date().toTimeString()), 1000);
 
 setInterval( () => io.emit('santa', JSON.stringify(mySanta.getPosition())), 1000);
 
